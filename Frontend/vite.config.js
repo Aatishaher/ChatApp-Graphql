0b4d4887411624587ff20b0580e824c2573ede52
Aatish Aher <aatishaher46@gmail.com>
2025-05-07 17:17:54 +0530

    version

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['jwt-decode']
-  }
+  },
+  build: {
+    rollupOptions: {
+      external: ['jwt-decode'],
+    },
+  },
 })
